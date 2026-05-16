cc7e775b0e0af6816e56f51973bb779b243ef19d
Chang Liu <24112359@student.uwa.edu.au>
2026-05-16 19:23:05 +0800

    Fixing error
    Co-authored-by: Mars-Martiny <23121354@student.uwa.edu.au>

--- modules/training_loop/__init__.py
+++ modules/training_loop/__init__.py
@@ -3,7 +3,7 @@
 from .config import _build_train_config
 from .train_loop import train_model_loop
 
-__all__ = ["training", "_build_train_config", "train_model_loop", ]
+__all__ = ["training", "_build_train_config", "train_model_loop"]
 
 """
 TRAINING LOOP MODULE // Files related to the main training loop and its components
